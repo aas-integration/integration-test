1bdaac37725e96c7ca0a1c8fa787c021af18a8cc
Jianchu <jianchuli0624@gmail.com>
2016-04-20 17:52:30 -0400

    infer sequence for the examples in corpus, ignore generated files from inference tool

--- type_inference/run.sh
+++ type_inference/run.sh
@@ -2,8 +2,7 @@
 CORPUS_DIR=../../corpus
 SCRIPT=$(readlink -f $0)
 MYDIRPATH=`dirname $SCRIPT`
-#DLJC=$MYDIRPATH/do-like-javac
-DLJC_BIN=$MYDIRPATH/pascali-public/do-like-javac/bin
+DLJC=$MYDIRPATH/do-like-javac
 
 if [ -d "generic-type-inference-solver" ]; then
   (cd generic-type-inference-solver && git pull)
@@ -11,29 +10,28 @@ else
   git clone https://github.com/Jianchu/generic-type-inference-solver.git
 fi
 
-#if [ -d "do-like-javac" ]; then
-if [ -d "pascali-public" ]; then
-  (cd do-like-javac && git pull)
+if [ -d "do-like-javac" ]; then
+  (cd do-like-javac && git pull && git checkout checker)
 else
-  #git clone https://github.com/SRI-CSL/do-like-javac.git
-  git clone https://github.com/Jianchu/pascali-public.git
+  git clone https://github.com/SRI-CSL/do-like-javac.git
+  (cd do-like-javac && git checkout checker)
 fi
 
 cd generic-type-inference-solver
 export TRAVIS_BUILD_DIR=`pwd`
 
 ./.travis-build-without-test.sh
-
+rm -rf $CORPUS_DIR/annotated/
 for f in $CORPUS_DIR/*
 do
   if [ -d "$f" ]; then
-  	cd $f
-  	ant clean
-  	echo "Inferring ${PWD##*/}:"
-    python $DLJC_BIN/do-like-javac.py -t inference --solverArgs="backEndType=maxsatbackend.MaxSat" --checker ontology.OntologyChecker --solver constraintsolver.ConstraintSolver -o logs -m ROUNDTRIP -afud $CORPUS_DIR/annotated -- ant
+    cd $f
+    ant clean
+    echo "Inferring ${PWD##*/}:"
+    python $DLJC/dljc -t inference --solverArgs="backEndType=maxsatbackend.MaxSat" --checker ontology.OntologyChecker --solver constraintsolver.ConstraintSolver -o logs -m ROUNDTRIP -afud $CORPUS_DIR/annotated -- ant
   fi
 done
 
-# Sort03 causes checker framework inference crushing, 
+# Sort03 causes checker framework inference crushing,
 # the problem has been filed as Issue #24 in checker framework inference.
 cd ..
