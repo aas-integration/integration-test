8f15be22a6cd696ae098a1e3b665d872909b875c
Jianchu <jianchuli0624@gmail.com>
2016-04-19 20:08:08 -0400

    infer corpus

--- type_inference/run.sh
+++ type_inference/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-CORPUS_DIR=../../corpus/benchmarks
-CORPUS_SORT1_SRC_DIR=../../corpus/sorting/00_sort/Sort01/src
-CORPUS_SORT2_SRC_DIR=../../corpus/sorting/00_sort/Sort02/src
-CORPUS_SORT3_SRC_DIR=../../corpus/sorting/00_sort/Sort03/src
+CORPUS_DIR=../../corpus
+SCRIPT=$(readlink -f $0)
+MYDIRPATH=`dirname $SCRIPT`
+#DLJC=$MYDIRPATH/do-like-javac
+DLJC_BIN=$MYDIRPATH/pascali-public/do-like-javac/bin
 
 if [ -d "generic-type-inference-solver" ]; then
   (cd generic-type-inference-solver && git pull)
@@ -11,18 +11,29 @@ else
   git clone https://github.com/Jianchu/generic-type-inference-solver.git
 fi
 
+#if [ -d "do-like-javac" ]; then
+if [ -d "pascali-public" ]; then
+  (cd do-like-javac && git pull)
+else
+  #git clone https://github.com/SRI-CSL/do-like-javac.git
+  git clone https://github.com/Jianchu/pascali-public.git
+fi
+
 cd generic-type-inference-solver
 export TRAVIS_BUILD_DIR=`pwd`
 
 ./.travis-build-without-test.sh
 
-echo "Inferring example Sort01"
-bash scripts/ontology/roundtripOntologyMaxSat.sh $CORPUS_SORT1_SRC_DIR/*.java
-echo "Inferring example Sort02"
-bash scripts/ontology/roundtripOntologyMaxSat.sh $CORPUS_SORT2_SRC_DIR/*.java
-# Following example causes checker framework inference crushing, 
-# the problem has been filed as Issue #24 in checker framework inference.
+for f in $CORPUS_DIR/*
+do
+  if [ -d "$f" ]; then
+  	cd $f
+  	ant clean
+  	echo "Inferring ${PWD##*/}:"
+    python $DLJC_BIN/do-like-javac.py -t inference --solverArgs="backEndType=maxsatbackend.MaxSat" --checker ontology.OntologyChecker --solver constraintsolver.ConstraintSolver -o logs -m ROUNDTRIP -afud $CORPUS_DIR/annotated -- ant
+  fi
+done
 
-# echo "Inferring example Sort03"
-# bash scripts/ontology/roundtripOntologyMaxSat.sh $CORPUS_SORT3_SRC_DIR/*.java
+# Sort03 causes checker framework inference crushing, 
+# the problem has been filed as Issue #24 in checker framework inference.
 cd ..
