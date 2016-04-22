f7685d1662c965622b2ea1a08bb992312b2e4c2e
hsanchez <huascar.sanchez@gmail.com>
2016-04-22 16:01:21 -0700

    adds more examples

---
+++ corpus/Sort16/src/Sort16.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Sort16 {
+  public static void main(String[] args) {
+    String[] fruits = new String[] {"Pineapple","Apple", "Orange", "Banana"};
+
+    sort(fruits);
+
+    int i=0;
+    for(String temp: fruits){
+      System.out.println("fruits " + (++i) + " : " + temp);
+    }
+  }
+
+  private static void sort(String[] fruits){
+    Arrays.sort(fruits);
+  }
+}
