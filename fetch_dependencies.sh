17325f6c481668f191e50ac90f468df638579e7d
Tim McCarthy <thoomfish@gmail.com>
2016-04-26 14:23:30 -0700

    Refactoring and integrate inference tool
    - Move do-like-javac into tools subdirectory
    - Add inference dependencies to fetch_dependencies, downloading to tools
    - Moved some code from backend into common
    - Made generic run_dljc function in common
    - Refactored backend/frontend to consistently pass around "project"
      references instead of "project_dir"
    
    NOTE: inference tool isn't fully working yet

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR &> /dev/null
 
+# Libraries
 mkdir -p libs
 pushd libs &> /dev/null
 
@@ -14,7 +15,6 @@ JARS=(
     "http://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar"
     "https://github.com/petablox-project/petablox/releases/download/v1.0/petablox.zip"
 )
-# Download .jars
 
 for jar in "${JARS[@]}"
 do
@@ -27,19 +27,35 @@ done
 unzip -o petablox.zip
 rm petablox.zip
 
-popd &> /dev/null
+popd &> /dev/null # Exit libs
+
+
+# Tools
+mkdir -p tools
+pushd tools &> /dev/null
 
 # Fetch do-like-javac
 if [ ! -d do-like-javac ]; then
     git clone https://github.com/SRI-CSL/do-like-javac.git
     pushd do-like-javac &> /dev/null
     git checkout checker
-    popd &> /dev/null
+    popd &> /dev/null # Exit do-like-javac
 else
     pushd do-like-javac &> /dev/null
     git pull
-    popd &> /dev/null
+    popd &> /dev/null # Exit do-like-javac
 fi
 
+if [ ! -d "generic-type-inference-solver" ]; then
+    git clone https://github.com/Jianchu/generic-type-inference-solver.git
+fi
 
-popd &> /dev/null
+pushd generic-type-inference-solver &> /dev/null
+git pull
+export TRAVIS_BUILD_DIR=`pwd`
+./.travis-build-without-test.sh
+popd &> /dev/null # Exit generic-type-inference-solver
+
+popd &> /dev/null # Exit tools
+
+popd &> /dev/null # Exit integration-test
--- frontend.py
+++ frontend.py
@@ -1,6 +1,4 @@
-import os, sys, shutil
-import subprocess
-import traceback
+import os, shutil
 
 import inv_check
 import insert_jaif
@@ -9,33 +7,59 @@ import ontology_to_daikon
 import backend
 import common
 
-WORKING_DIR = os.path.dirname(os.path.realpath(__file__))
-daikon_jar = os.path.join(WORKING_DIR, "libs/daikon.jar") 
+def run_inference(project):
+  project_dir = common.get_project_dir(project)
+  annotation_dir = os.path.join(project_dir, common.DLJC_OUTPUT_DIR, 'annotations')
 
+  os.environ['JSR308'] = common.TOOLS_DIR
+  classpath = os.path.join(common.TOOLS_DIR, 'generic-type-inference-solver', 'bin')
+  if os.environ.get('CLASSPATH'):
+    os.environ['CLASSPATH'] += ':' + classpath
+  else:
+    os.environ['CLASSPATH'] = classpath
+
+  afu = os.path.join(common.TOOLS_DIR, 'annotation-tools', 'annotation-file-utilities')
+  os.environ['AFU'] = afu
+  os.environ['PATH'] += ':' + os.path.join(afu, 'scripts')
+
+  if os.path.isdir(annotation_dir):
+    shutil.rmtree(annotation_dir)
+
+  with common.cd(project_dir):
+    common.clean_project(project)
+    common.run_dljc(project,
+                    ['inference'],
+                    ['--solverArgs=backendType=maxsatbackend.MaxSat',
+                     '--checker', 'ontology.OntologyChecker',
+                     '--solver', 'constraintsolver.ConstraintSolver',
+                     '-m', 'ROUNDTRIP',
+                     '-afud', annotation_dir])
 
 def main():
   """ SUMMARY: use case of the user-driven functionality of PASCALI.
   Scenario: User provides the concept of Sequence and the equivalent Java
   types, and the concept of sorted sequence and the relevant type invariant.
-  Goal: learn how to get from Sequence -> Sorted Sequence. 
+  Goal: learn how to get from Sequence -> Sorted Sequence.
   """
 
-  """ Look for new mapping from 'ontology concepts'->'java type' and run 
+  corpus = common.get_project_list()
+
+  """ Look for new mapping from 'ontology concepts'->'java type' and run
   checker framework. Should be implemented in type_inference
   Mapping example:
     Sequence -> java.lang.Array, java.util.List, LinkedHashSet, etc.
 
   INPUT: corpus, file containing set of concept->java_type mapping
   OUTPUT: Set of jaif files that are merged into the classes using
-          insert_jaif. 
+          insert_jaif.
   BODY: This also triggers back-end labeled graph generation.
   """
 
-  print "todo" #WERNER
-
+  # for project in corpus:
+  #   run_inference(project)
 
   """ Missing step: interact with PA to add a definition of Sorted Sequence
-  which is a specialization of Sequence that has a sortedness invariants. 
+  which is a specialization of Sequence that has a sortedness invariants.
   The sortedness invariant gets turned into a Daikon template
   INPUT: user interaction
   OUTPUT: type_annotation and type_invariant (for sorted sequence)
@@ -45,7 +69,7 @@ def main():
   ontology_invariant_file = "TODO_from_Howie.txt"
   with open(ontology_invariant_file, 'w') as f:
     f.write("TODO")
-  
+
   invariant_name = "TODO_sorted_sequence"
 
   daikon_pattern_java_file = ontology_to_daikon.create_daikon_invariant(ontology_invariant_file, invariant_name)
@@ -55,24 +79,23 @@ def main():
 
   INPUT: dtrace file of project
          daikon_pattern_java_file that we want to check on the dtrace file.
-  
-  OUTPUT: list of ppt names that establish the invariant. Here a ppt 
+
+  OUTPUT: list of ppt names that establish the invariant. Here a ppt
   is a Daikon program point, s.a. test01.TestClass01.sort(int[]):::EXIT
 
-  Note: this step translate the type_invariant into a Daikon 
+  Note: this step translate the type_invariant into a Daikon
   template (which is a Java file).
   """
 
   pattern_class_name = invariant_name
-  pattern_class_dir = os.path.join(WORKING_DIR, "invClass")
+  pattern_class_dir = os.path.join(common.WORKING_DIR, "invClass")
   if os.path.isdir(pattern_class_dir):
     shutil.rmtree(pattern_class_dir)
   os.mkdir(pattern_class_dir)
 
-  cmd = ["javac", "-g", "-classpath", daikon_jar, daikon_pattern_java_file, "-d", pattern_class_dir]
+  cmd = ["javac", "-g", "-classpath", common.get_jar('daikon.jar'),
+         daikon_pattern_java_file, "-d", pattern_class_dir]
   common.run_cmd(cmd)
-
-  corpus = backend.get_project_list() #TODO: @Tim, add the real corpus here
 
   list_of_methods = []
   for project in corpus:
@@ -92,9 +115,9 @@ def main():
         print("\t{}".format(m))
   print ("\n   ************")
 
-  """ Expansion of dynamic analysis results .... 
+  """ Expansion of dynamic analysis results ....
   Find a list of similar methods that are similar to the ones found above (list_of_methods).
-  INPUT: list_of_methods, corpus with labeled graphs generated, threshold value for similarity, 
+  INPUT: list_of_methods, corpus with labeled graphs generated, threshold value for similarity,
   OUTPUT: superset_list_of_methods
   """
 
@@ -104,9 +127,9 @@ def main():
   INPUT: superset_list_of_methods, annotation to be added
   OUTPUT: nothing
   EFFECT: updates the type annotations of the methods in superset_list_of_methods.
-  This requires some additional checks to make sure that the methods actually 
+  This requires some additional checks to make sure that the methods actually
   perform some kind of sorting. Note that we do it on the superset because the original
-  list_of_methods might miss many implementations because fuzz testing could not 
+  list_of_methods might miss many implementations because fuzz testing could not
   reach them.
   """
   for class_file in []: # MARTIN
@@ -117,18 +140,18 @@ def main():
   """ Ordering of expanded dynamic analysis results ....
   Find the k 'best' implementations in superset of list_of_methods
   INPUT: superset_list_of_methods, corpus, k
-  OUTPUT: k_list_of_methods 
+  OUTPUT: k_list_of_methods
   Note: similarity score is used. may consider using other scores; e.g., TODO:???
   """
 
   print "todo" # Huascar
 
-  """ 
+  """
   Close the loop and add the best implementation found in the previous
   step back to the ontology.
   INPUT: k_list_of_methods
   OUTPUT: patch file for the ontology. Worst case: just add the 'best' implementation
-  found in the corpus as a blob to the ontology. Best case: generate an equivalent 
+  found in the corpus as a blob to the ontology. Best case: generate an equivalent
   flow-graph in the ontology.
   """
   print "TODO" # ALL
