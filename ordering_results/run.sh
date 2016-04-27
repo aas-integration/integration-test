011dd17249c27e68438265e1b0b87342eec0bf78
hsanchez <huascar.sanchez@gmail.com>
2016-04-27 11:47:39 -0700

    adds the -t option, which allow callers of this run.sh to choose which component to run; e.g., -t typicality or -t concepts

--- ordering_results/run.sh
+++ ordering_results/run.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
 
-echo "Finds k most typical implementation in corpus."
-# "./cue typical -d $CORPUS_DIR"
-./cue typical $*  
 
-echo "Extracts concepts from corpus."
-./cue concepts $*
+KMOST=10
+
+# thx to http://stackoverflow.com/questions/192249/
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -f|--file)
+    FILE="$2"
+    shift # past argument
+    ;;
+    -t|--tool)
+    TOOL="$2"
+    shift # past argument
+    ;;
+    -k|--topk)
+    KMOST="$2"
+    shift # past argument
+    ;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [[ -n "$TOOL" ]] && [[ "${TOOL}" == "concepts" ]]; 
+then
+	echo "Extracts concepts from corpus."	
+	./cue concepts -k $KMOST -f $FILE	
+else
+	if [[ -n "$TOOL" ]] && [[ "${TOOL}" == "typicality" ]]; then
+		echo "Finds k most typical implementation in corpus."
+		./cue typical -k $KMOST -f $FILE
+	else	
+		echo "Runs both typicality and concepts commands"
+		./cue typical -k $KMOST -f $FILE		
+		./cue concepts -k $KMOST -f $FILE		
+	fi
+fi	
