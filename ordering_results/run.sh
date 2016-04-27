6ae4c039972422131526adadb5bdca887c4d8be1
hsanchez <huascar.sanchez@gmail.com>
2016-04-26 22:02:04 -0700

    updates CUE's cli

--- ordering_results/run.sh
+++ ordering_results/run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-CORPUS_DIR=../corpus/
-
-K=1
-M=methods.txt
-
 echo "Finds k most typical implementation in corpus."
 # "./cue typical -d $CORPUS_DIR"
-./cue typical -d $CORPUS_DIR 
+./cue typical $*  
 
 echo "Extracts concepts from corpus."
-./cue concepts -k 2 -m $M -f $CORPUS_DIR
+./cue concepts $*
