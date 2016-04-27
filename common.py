28f941b0d4ca616160b57899b62c713b604a7866
Tim McCarthy <thoomfish@gmail.com>
2016-04-26 17:31:39 -0700

    Fixed typo

--- common.py
+++ common.py
@@ -46,7 +46,7 @@ def get_project_dir(project_name):
   return os.path.join(CORPUS_DIR, project_name)
 
 def get_project_list():
-  return [project for project in  os.listdir(corpus_dir) if os.path.isdir(get_project_dir(project))]
+  return [project for project in  os.listdir(CORPUS_DIR) if os.path.isdir(get_project_dir(project))]
 
 def clean_project(project):
   project_dir = get_project_dir(project)
